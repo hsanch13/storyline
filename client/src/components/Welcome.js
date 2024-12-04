74298dd00918bfd836878d4f5ef1a08edeb19314
hsanch13 <hayleysanchez13@gmail.com>
2024-12-04 10:07:21 -0700

    updated nav bar, welcome page and routes to reflect only two forms

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -9,7 +9,7 @@ function Welcome() {
     <div>
       <h1>Welcome to Storyline!</h1>
         <button onClick={() => navigate("/view-all")}>View all stories and sources</button>
-        <button onClick={() => navigate("/add-story-source")}>Add a new story and source</button>
+        {/* <button onClick={() => navigate("/add-story-source")}>Add a new story and source</button> */}
         <button onClick={() => navigate("/add-story")}>Add a new story</button>
         <button onClick={() => navigate("/add-source")}>Add a new source</button>
     </div>
