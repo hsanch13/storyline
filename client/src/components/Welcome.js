a3978e247e8147d26b090995110d8c35e34800be
hsanch13 <hayleysanchez13@gmail.com>
2024-12-06 10:38:21 -0700

    updated button and arrangement of buttons on welcome comp

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -5,40 +5,41 @@ import { useNavigate } from 'react-router-dom';
 function Welcome() {
   const navigate = useNavigate();
 
-  return (
-    <div className="container text-center my-5">
-      <h1 className="display-4 mb-4">Welcome to Storyline!</h1>
-      <div className="d-grid gap-2 col-6 mx-auto">
-        <a 
-          href="/view-all" 
-          onClick={(e) => {
-            e.preventDefault(); // Prevent default anchor behavior
-            navigate("/view-all");
-          }}
-        >
-          <span>View all stories and sources</span>
-        </a>
-        <a 
-          href="/add-story" 
-          onClick={(e) => {
-            e.preventDefault();
-            navigate("/add-story");
-          }}
-        >
-          <span>Add a new story</span>
-        </a>
-        <a 
-          href="/add-source" 
-          onClick={(e) => {
-            e.preventDefault();
-            navigate("/add-source");
-          }}
-        >
-          <span>Add a new source</span>
-        </a>
-      </div>
+return (
+  <div className="welcome-container">
+    <h1 className="display-4 mb-4">Welcome to Storyline!</h1>
+    <div className="d-grid gap-2 col-6 mx-auto">
+      <a 
+        href="/view-all" 
+        onClick={(e) => {
+          e.preventDefault(); // Prevent default anchor behavior
+          navigate("/view-all");
+        }}
+      >
+        <span>View all stories and sources</span>
+      </a>
+      <a 
+        href="/add-story" 
+        onClick={(e) => {
+          e.preventDefault();
+          navigate("/add-story");
+        }}
+      >
+        <span>Add a new story</span>
+      </a>
+      <a 
+        href="/add-source" 
+        onClick={(e) => {
+          e.preventDefault();
+          navigate("/add-source");
+        }}
+      >
+        <span>Add a new source</span>
+      </a>
     </div>
-  );
+  </div>
+);
+
 }  
 
 export default Welcome;
